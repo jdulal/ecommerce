00333b7b9024764c61f24bd75e4c2317ce632749
Jagadishwor Dulal <jdulal@yahoo.com>
2019-05-12 00:33:16 +0545

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+
+void main() => runApp(MyHome());
+
+class MyHome extends StatefulWidget {
+  MyHome({Key key}) : super(key: key);
+
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget imageCarousel=new Container(
+    height: 225.0,
+    child: Carousel(
+      overlayShadow: false,
+      borderRadius: true,
+      boxFit: BoxFit.cover,
+      autoplay: true,
+      dotSize: 5.0,
+      indicatorBgPadding: 9.0,
+      images: [
+        new AssetImage('assets/slider/slider1.jpg'),
+        new AssetImage('assets/slider/slider2.jpg'),
+        new AssetImage('assets/slider/slider3.jpg'),
+        new AssetImage('assets/slider/slider4.jpg'),
+        new AssetImage('assets/slider/slider5.jpg'),
+        new AssetImage('assets/slider/slider6.jpg'),
+      ],
+      animationCurve: Curves.fastOutSlowIn,
+      animationDuration: Duration(microseconds: 1500),
+    ),
+  );
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text('Braces e-Commerce'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search,color:Colors.white,),
+            onPressed: null,
+            ),
+            IconButton(icon: Icon(Icons.shopping_basket,color:Colors.white),
+            onPressed: (){
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  //builder: (BuildContext context)=> Cart(),
+              ),
+              );
+            },
+            ),
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            SizedBox(height: 5.0,),
+            imageCarousel,
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
